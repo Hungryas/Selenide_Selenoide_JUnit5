f4dc0a9e13a337e75b5f41d80dd63e158ca37313
User <hungryas@gmail.com>
2022-08-06 13:13:36 +0500

    patch: fix

--- src/test/java/pages/booking/SearchResultsPage.java
+++ src/test/java/pages/booking/SearchResultsPage.java
@@ -75,11 +75,12 @@ public class SearchResultsPage {
     public SearchResultsPage checkCardsCountFilteredByRating(Integer rating) {
         final String RATING_LOCATOR = String.format("[data-filters-item='class:class=%s']", rating);
         FILTERS_SIDEBAR.find(RATING_LOCATOR).scrollTo().click();
-        if (OVERLAY_CARD.isDisplayed()) {OVERLAY_CARD.shouldBe(disappear, ofSeconds(8));}
+        if (OVERLAY_CARD.isDisplayed()) {OVERLAY_CARD.shouldBe(disappear, ofSeconds(6));}
 
         final String CARD_RATING_LOCATOR = String.format(".//*[contains(@data-testid,'rating')][count(span)=%s]", rating);
         int expectedCardsCount = SEARCH_RESULTS_TABLE.findAll("[data-testid='property-card']").size();
-        int actualCardsCount = SEARCH_RESULTS_TABLE.findAll(By.xpath(CARD_RATING_LOCATOR)).size();
+        int actualCardsCount = SEARCH_RESULTS_TABLE.findAll(By.xpath(CARD_RATING_LOCATOR))
+                .shouldHave(sizeGreaterThan(0)).size();
         assertEquals(expectedCardsCount, actualCardsCount);
         return this;
     }
